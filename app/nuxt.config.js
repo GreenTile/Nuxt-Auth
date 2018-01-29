415c7d2908f84165ef6a1d5eff3741c02cc7a6a4
Mohammad MDSA <samimd.77@gmail.com>
2018-01-29 23:29:30 +0330

    Setup app

---
+++ app/nuxt.config.js
@@ -0,0 +1,30 @@
+const { resolve } = require('path')
+
+const themeColor = 'red'
+
+module.exports = {
+  mode: 'spa',
+
+  srcDir: __dirname,
+  buildDir: resolve(__dirname, '.nuxt'),
+
+  modules: [
+    // '@nuxtjs/auth',
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+  ],
+
+  plugins: [ ],
+
+  manifest: {
+    lang: 'en',
+    name: 'music app',
+    short_name: 'musicapp',
+    description: 'best music app',
+    theme_color: themeColor
+  },
+
+  proxy: [
+    process.env.PROXY_API_URL || 'http://localhost:3000/api'
+  ]
+}
